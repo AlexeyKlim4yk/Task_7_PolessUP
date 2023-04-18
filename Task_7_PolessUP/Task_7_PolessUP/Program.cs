403ce67606731b8950914e671a19a5b3372471aa
AlexeyKlim4yk <114896207+AlexeyKlim4yk@users.noreply.github.com>
2023-04-18 17:45:15 +0300

    Add files via upload

---
+++ Task_7_PolessUP/Task_7_PolessUP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_7_PolessUP
+{
+    class Program
+    {
+        static void NearNumeric(int FirsrtNumeric)
+        {
+            List<int> NumericalSymbols = new List<int> { };
+
+            while (FirsrtNumeric != 0)
+            {
+                NumericalSymbols.Add(FirsrtNumeric % 10);
+                FirsrtNumeric /= 10;
+            }
+            
+            NumericalSymbols.Sort();
+            NumericalSymbols.Reverse();
+
+            string MaxNumeric = "";
+            foreach (int x in NumericalSymbols)
+            {
+                MaxNumeric += x;
+            }
+            Console.WriteLine(MaxNumeric);
+        }
+        static void Main(string[] args)
+        {
+            NearNumeric(71273);
+        }
+    }
+}
